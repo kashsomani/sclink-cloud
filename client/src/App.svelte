95fbf73bf157d955be40fb58133dad412c0843c8
Kash Somani <kash@kashsomani.com>
2022-02-21 14:04:34 -0800

    added navbar

---
+++ client/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Navbar from './components/Navbar.svelte'
+</script>
+<svelte:head>
+	<link href='https://fonts.googleapis.com/css?family=Roboto Condensed' rel='stylesheet'>
+</svelte:head>
+<main>
+	<Navbar/>
+</main>
+
+<style lang="postcss" global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
+	main{
+		color: white;
+		background-color: #14132C;
+		@apply p-0 m-0 h-full;
+	}
+</style>
---
+++ client/src/components/Navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+  // your script goes here
+</script>
+
+<div id="container">
+  <div>S<span class="logo-c">C</span>LINK</div>
+</div>
+
+<style lang="postcss">
+  #container{
+    background-color: #054970;
+    @apply p-2 m-0 h-16 grid grid-cols-12 place-items-center;
+  }
+  .logo-c{
+    font-family: 'Stick No Bills';
+  }
+</style>

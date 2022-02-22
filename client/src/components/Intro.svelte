1535744af8e1385fd1569bf2012254f2da6f215e
Kash Somani <kash@kashsomani.com>
2022-02-22 14:03:24 -0800

    added rain

--- client/src/components/Intro.svelte
+++ client/src/components/Intro.svelte
@@ -1,8 +1,16 @@
 <script>
-
+  import Rain from './Rain.svelte'
 </script>
 
 <style lang="postcss" scoped>
+  #main{
+    position: relative;
+  }
+  #rain{
+    position: absolute;
+    @apply w-full;
+    z-index: -1;
+  }
   img {
     @apply w-1/4;
   }
@@ -25,19 +33,24 @@
   }
 </style>
 
-<div id="container">
-  <div>
-    <img src="./cloud.png" alt="cloud" />
+<div id="main">
+  <div id="rain">
+    <Rain />
   </div>
-  <div id="intro-text">
-    <h1>
-      LINKING THE
-      <br />
-      <span class="emphasis">CLOUD</span>
-      <br />
-      TO
-      <br />
-      <span class="emphasis">YOUR SCHOOL</span>
-    </h1>
+  <div id="container">
+    <div>
+      <img src="./cloud.png" alt="cloud" />
+    </div>
+    <div id="intro-text">
+      <h1>
+        LINKING THE
+        <br />
+        <span class="emphasis">CLOUD</span>
+        <br />
+        TO
+        <br />
+        <span class="emphasis">YOUR SCHOOL</span>
+      </h1>
+    </div>
   </div>
 </div>

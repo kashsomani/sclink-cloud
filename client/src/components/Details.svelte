4893fc3f3697a2e16a2399ed25a570cfd140f3ea
Kash Somani <kash@kashsomani.com>
2022-02-21 16:04:52 -0800

    details component

---
+++ client/src/components/Details.svelte
@@ -0,0 +1,15 @@
+<div id="container">
+  <div class="heading">
+    <h2>&nbsp; &nbsp; Our Mission</h2>
+  </div>
+  <div class="detail-text">hi</div>
+</div>
+
+<style lang="postcss" scoped>
+  #container {
+    @apply p-16 text-xl;
+  }
+  .heading {
+    border-bottom: 3px solid #00969b;
+  }
+</style>

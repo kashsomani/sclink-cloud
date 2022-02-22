3dd09aee6986365364fe74a7886607c70cf73228
Kash Somani <kash@kashsomani.com>
2022-02-21 16:04:43 -0800

    imports, formatting, minor styles

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,11 +1,19 @@
 <script>
-	import Navbar from './components/Navbar.svelte'
+  import Navbar from "./components/Navbar.svelte";
+  import Intro from "./components/Intro.svelte";
+  import Details from "./components/Details.svelte";
 </script>
+
 <svelte:head>
-	<link href='https://fonts.googleapis.com/css?family=Roboto Condensed' rel='stylesheet'>
+  <link
+    href="https://fonts.googleapis.com/css?family=Roboto Condensed"
+    rel="stylesheet"
+  />
 </svelte:head>
 <main>
-	<Navbar/>
+  <Navbar />
+  <Intro />
+  <Details />
 </main>
 
 <style lang="postcss" global>
@@ -13,9 +21,9 @@
   @tailwind components;
   @tailwind utilities;
 
-	main{
-		color: white;
-		background-color: #14132C;
-		@apply p-0 m-0 h-full;
-	}
+  main {
+    color: white;
+    background-color: #0e1535;
+    @apply p-0 m-0 h-full;
+  }
 </style>

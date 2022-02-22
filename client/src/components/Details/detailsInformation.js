b628aff6cf09a6f51d4f154cbf3efd04ce105de4
Kash Somani <kash@kashsomani.com>
2022-02-21 16:47:36 -0800

    refactored text for sections

---
+++ client/src/components/Details/detailsInformation.js
@@ -0,0 +1,17 @@
+export const detailsInformation = [
+  {
+    heading: '&nbsp Our Mission',
+    text: `
+      &nbsp &nbsp The world is in the constant process of adopting cloud technologies to solve
+      business problems. This has especially been true of the online era COVID-19
+      has triggered. 
+      <br> 
+      &nbsp &nbsp We aim to equip schools to leverage cloud-based technologies
+      to not only make schools more effient and better equipped, but to also
+      strengthen their brand and reach.`,
+  },
+  {
+    //must be last
+    heading: 'Our Products',
+    text: ''},
+]

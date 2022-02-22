f7d85ed02464e3073b52ab4f6af9e44e10451432
Kash Somani <kash@kashsomani.com>
2022-02-21 16:04:25 -0800

    added intro

---
+++ client/src/components/Intro.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+<div id="container">
+  <div><img src="./cloud.png" alt="cloud" /></div>
+  <div id="intro-text">
+    <h1>
+      BRINGING THE <br /> <span class="emphasis">CLOUD</span> <br /> TO <br />
+      <span class="emphasis">YOUR SCHOOL</span>
+    </h1>
+  </div>
+</div>
+
+<style lang="postcss" scoped>
+  img {
+    @apply w-1/4;
+  }
+  #intro-text {
+    color: white;
+    @apply grid grid-cols-1 place-items-center;
+    @apply text-center text-2xl;
+    @apply p-4;
+  }
+  .emphasis {
+    font-size: xxx-large;
+    font-weight: 800;
+    color: #00969b;
+  }
+  div {
+    @apply grid grid-cols-1 place-items-center;
+  }
+  #container {
+    @apply p-2 m-6;
+  }
+</style>

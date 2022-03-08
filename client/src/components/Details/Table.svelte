82bcb555158ead4eef10261640d9ad657047d00d
Kash Somani <kash@kashsomani.com>
2022-03-07 20:53:20 -0800

    made new table component

---
+++ client/src/components/Details/Table.svelte
@@ -0,0 +1,96 @@
+<script>
+  import { tick } from 'svelte'
+
+  let tableData = [
+    {
+      '': 'Webpage',
+      Nano: true,
+      Micro: true,
+      Mega: true,
+      Giga: true,
+    },
+    {
+      '': 'Email',
+      Nano: false,
+      Micro: true,
+      Mega: true,
+      Giga: true,
+    },
+    {
+      '': 'Database',
+      Nano: false,
+      Micro: false,
+      Mega: true,
+      Giga: true,
+    },
+    {
+      '': 'Cloud Email',
+      Nano: false,
+      Micro: false,
+      Mega: true,
+      Giga: true,
+    },
+    {
+      '': 'WhatsApp',
+      Nano: false,
+      Micro: false,
+      Mega: false,
+      Giga: true,
+    },
+  ]
+</script>
+
+<style lang="postcss" scoped>
+  #table-container {
+    background-color: rgba(52, 72, 73, 0.25);
+    @apply grid grid-cols-1 place-items-center text-center p-2 text-base md:text-xl m-4 rounded-lg;
+  }
+  table td,
+  table th {
+    border-right: 1px solid #00818a;
+    @apply p-2 py-3;
+  }
+  table th {
+    border-bottom: 1px solid #00818a;
+    @apply px-3;
+  }
+  table :global(img) {
+    @apply h-6 w-9 md:h-8 md:w-12;
+  }
+  table :global(.img-container) {
+    @apply grid grid-cols-1 place-items-center p-1 md:p-6;
+  }
+  #extra-th {
+    border-right: none;
+  }
+</style>
+
+<div id="table-container">
+
+  <table>
+    <thead>
+      <tr>
+        {#each Object.keys(tableData[0]) as columnHeading}
+          <th>{columnHeading}</th>
+        {/each}
+        <th id="extra-th">&nbsp;&nbsp;&nbsp;</th>
+      </tr>
+      <tr />
+    </thead>
+    <tbody>
+      {#each Object.values(tableData) as row}
+        <tr>
+          {#each Object.values(row) as cell}
+            <td>
+              {@html cell === true ? `
+                <div class="img-container">
+                  <img src="./tick.png"/>
+                </div>
+                ` : cell === false ? '' : cell}
+            </td>
+          {/each}
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
--- client/src/components/Details/index.svelte
+++ client/src/components/Details/index.svelte
@@ -1,14 +1,12 @@
 <script>
   import { detailsInformation } from './detailsInformation.js'
   import ProductDetails from './ProductDetails.svelte'
+  import Table from './Table.svelte'
 </script>
 
 <style lang="postcss" scoped>
   .detail-text {
     @apply  ml-8 mb-6;
-  }
-  img{
-    @apply h-3;
   }
   #container {
     @apply mx-0 text-xl pb-2;
@@ -16,20 +14,6 @@
   .heading {
     border-bottom: 3px solid #00969b;
     @apply mx-4 mb-2;
-  }
-  .table-heading {
-    border-bottom: 3px solid #00969b;
-    @apply mb-0 pb-0;
-  }
-  .table{
-    @apply grid grid-rows-5 gap-0 space-y-0 place-items-center text-sm py-0 my-0 mx-4;
-  }
-  .row{
-    @apply grid grid-cols-5 place-items-center my-0 py-0 h-full w-full gap-0 space-y-0;
-  }
-  .first{
-    border-right: 3px solid #00969b;
-    @apply mx-0 my-0 pt-2 px-2 min-w-full min-h-full;
   }
 </style>
 
@@ -44,50 +28,7 @@
       {@html detail.text}
     </div>
     {#if detail.table}
-    <div class="table">
-      <div class="row table-heading">
-        <div class="first"></div>
-        <div>Nano</div>
-        <div>Micro</div>
-        <div>Mega</div>
-        <div>Giga</div>
-      </div>
-      <div class="row">
-        <div class="first">Webpage</div>
-        <div><img src="./tick.png" alt="tick"></div>
-        <div><img src="./tick.png" alt="tick"></div>
-        <div><img src="./tick.png" alt="tick"></div>
-        <div><img src="./tick.png" alt="tick"></div>
-      </div>
-      <div class="row">
-        <div class="first">Email</div>
-        <div></div>
-        <div><img src="./tick.png" alt="tick"></div>
-        <div><img src="./tick.png" alt="tick"></div>
-        <div><img src="./tick.png" alt="tick"></div>
-      </div>
-      <div class="row">
-        <div class="first">Database</div>
-        <div></div>
-        <div></div>
-        <div><img src="./tick.png" alt="tick"></div>
-        <div><img src="./tick.png" alt="tick"></div>
-      </div>
-      <div class="row">
-        <div class="first">Email Sending</div>
-        <div></div>
-        <div></div>
-        <div><img src="./tick.png" alt="tick"></div>
-        <div><img src="./tick.png" alt="tick"></div>
-      </div>
-      <div class="row">
-        <div class="first">WhatsApp</div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div><img src="./tick.png" alt="tick"></div>
-      </div>
-    </div>
+    <Table/>
     {/if}
   {/each}
   <ProductDetails />
